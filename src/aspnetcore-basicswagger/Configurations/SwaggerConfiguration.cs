ae1c1dc1d5ebb8b8549f56f40f8eb55b5b311be9
Patrick Grasseels <grasseelsp@hotmail.com>
2018-02-28 20:28:24 +0100

    Update swagger conf

---
+++ src/aspnetcore-basicswagger/Configurations/SwaggerConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.PlatformAbstractions;
+using Swashbuckle.AspNetCore.Swagger;
+using System.IO;
+
+namespace AspnetCoreBasicSwagger.Configurations
+{
+    public static class SwaggerConfiguration
+    {
+        public static IServiceCollection AddSwaggerConfiguration(this IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "AspnetCoreBasicSwagger", Version = "v1" });
+                c.IncludeXmlComments(
+                    Path.Combine(
+                        PlatformServices.Default.Application.ApplicationBasePath, "aspnetcore_basicswagger.xml"));
+            });
+
+            return services;
+        }
+
+        public static IApplicationBuilder UseSwaggerConfiguration(this IApplicationBuilder app)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "AspnetCoreBasicSwagger V1");
+            });
+
+            return app;
+        }
+    }
+}
---
+++ src/aspnetcore-basicswagger/Controllers/ValuesController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace AspnetCoreBasicSwagger.Controllers
+{
+    /// <summary>
+    /// Test Comment For Controller
+    /// </summary>
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    public class ValuesController : Controller
+    {
+        /// <summary>
+        /// This method use the comment
+        /// </summary>
+        /// <returns>Return an array of string.</returns>
+        /// <response code="200">Return an array of string.</response>
+        /// <response code="500">Service unavailable.</response>
+        /// <response code="403">Service unauthorize.</response>
+        [HttpGet]
+        [SwaggerResponse(200, Type = typeof(string[]))]
+        [SwaggerResponse(500)]
+        [SwaggerResponse(403)]
+        public IActionResult Get()
+        {
+            return Ok(new string[] { "value1", "value2" });
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
